2453c7fc5f871278573b118d88e896a22a74b687
Ivo Perez Colo <ivoperezcolo@gmail.com>
2020-10-31 16:40:39 -0300

    Subo test lo sigo revisando...

--- tests/test_choose_action.py
+++ tests/test_choose_action.py
@@ -1,16 +1,29 @@
 import pytest
 import numpy as np
 
-Q = np.array([[0, 1, 2], [3, 4, 5]])
-epsilon = 0.10
+from simulai import sim
 
-# choose action
-def choose_action(row, p):
-    if p < (1 - epsilon):
-        i = np.argmax(Q[row, :])
-    else:
-        i = np.random.choice(Q.shape[0])
-    return i
+@pytest.fixture
+def var_input():
+	frame = "Modelo"
+	espera = sim.DiscreteVariable(
+		"Espera", 60, 300, 10, "Models."+frame+".espera")
+	stock = sim.DiscreteVariable(
+		 	"Stock", 10, 50, 10, "Models."+frame+".stock")
+	numviajes = sim.DiscreteVariable(
+		"Numero de viajes", 1, 5, 1, "Models."+frame+".numviajes")
+	vi = [espera, stock, numviajes]
+	
+	return vi
 
-def test_choose_action():
-	assert choose_action(1, 0.85) == 2
+def test_ini_saq(var_input):
+	my_method = sim.Qlearning(v_i=var_input, episodes_max=1, steps_max=10)
+	S, A, Q = my_method.ini_saq()
+
+	assert S.shape == (625, 3)
+
+def test_choose_action(var_input):
+	my_method = sim.Qlearning(v_i=var_input, episodes_max=1, steps_max=10)
+	_ = my_method.ini_saq()
+
+	assert my_method.choose_action(1) == 0
