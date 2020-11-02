86b3e5f53b2185f89979e3afbeb9f460613c7742
BerMPirozzo <ber_pirozzo@hotmail.com.ar>
2020-11-02 18:06:48 -0300

    Actualización de interface

--- simulai/interface.py
+++ simulai/interface.py
@@ -1,11 +1,10 @@
 
 # This file is part of the
-#   SimulAI Project (https://github.com/carosaav/SimulAI).
+# SimulAI Project (https://github.com/carosaav/SimulAI).
 # Copyright (c) 2020, Perez Colo Ivo, Pirozzo Manuel Bernardo,
 # Carolina Saavedra Sueldo
 # License: MIT
-#   Full Text: https://github.com/carosaav/SimulAI/blob/master/LICENSE
-
+# Full Text: https://github.com/carosaav/SimulAI/blob/master/LICENSE
 # ============================================================================
 # IMPORTS
 # ============================================================================
@@ -19,169 +18,138 @@ import os
 # COMMUNICATION
 # ============================================================================
 
+def check_connection(method):
+    """
+    This function checks the connection status, returning an error
+    menssaje if it fails
+    """
+    def wrapper(self, *args, **kwargs):
+        if self.is_connected is True:
+            self(*args, **kwargs)
+        else:
+            raise ConnectionError("Not connected:", self.name)
+        output = method(self, *args, **kwargs)
+        return output
+    return wrapper
 
 class Com(object):
+    """
+    Definition of the function of communication
+    """
+
     def __init__(self, model_name):
         self.model_name = model_name
         self.is_connected = False
-        self.plant_simulation = ""
+
 
     def get_path_file_model(self):
-        """Function that returns the complete file path.
+        """
+        Function that returns the complete file path
+        Input parameter: file name
+        Return: file path
         """
         path = os.getcwd() + "\\" + self.model_name
         return path
 
+    
     def connection(self):
-        """Function that returns the connection object.
+        """
+        Function that returns the connection object
+        Input parameter: file name
+        Return: connection object
         """
         path_file = self.get_path_file_model()
         try:
             self.plant_simulation = win32.Dispatch(
-                "Tecnomatix.PlantSimulation.RemoteControl.15.0"
-            )
+                "Tecnomatix.PlantSimulation.RemoteControl.15.0")
             self.plant_simulation.loadModel(path_file)
             print("The connection was successful")
             self.is_connected = True
             return True
-        except Exception:
+        except:
             print(("Connection error. path file: " + path_file))
             return False
 
+    @check_connection
     def setVisible(self, value):
-        if self.is_connected is True:
-            self.plant_simulation.setVisible(value)
-        else:
-            print("Not connected")
+        self.plant_simulation.setVisible(value)
 
+    @check_connection
     def setValue(self, ref, value):
-        if self.is_connected is True:
-            self.plant_simulation.setValue(ref, value)
-        else:
-            print("Not connected")
+        self.plant_simulation.setValue(ref, value)
 
+    @check_connection
     def getValue(self, ref):
-        if self.is_connected is True:
-            return self.plant_simulation.getValue(ref)
-        else:
-            print("Not connected")
+        return self.plant_simulation.getValue(ref)
 
+    @check_connection
     def startSimulation(self, ref):
-        if self.is_connected is True:
-            self.plant_simulation.startSimulation(ref)
-        else:
-            print("Not connected")
-
+        self.plant_simulation.startSimulation(ref)
+        
+    @check_connection
     def resetSimulation(self, ref):
-        if self.is_connected is True:
-            self.plant_simulation.resetSimulation(ref)
-        else:
-            print("Not connected")
+        self.plant_simulation.resetSimulation(ref)
 
+    @check_connection
     def stopSimulation(self, ref):
-        if self.is_connected is True:
-            self.plant_simulation.stopSimulation(ref)
-        else:
-            print("Not connected")
-
+        self.plant_simulation.stopSimulation(ref)
+    
+    @check_connection
     def closeModel(self):
-        if self.is_connected is True:
-            self.plant_simulation.CloseModel()
-        else:
-            print("Not connected")
-
+        self.plant_simulation.CloseModel()
+        
+    @check_connection
     def executeSimTalk(self, ref, value):
-        if self.is_connected is True:
-            self.plant_simulation.ExecuteSimTalk(ref, value)
-        else:
-            print("Not connected")
-
+        self.plant_simulation.ExecuteSimTalk(ref, value)
+    
+    @check_connection
     def isSimulationRunning(self):
-        if self.is_connected is True:
-            return self.plant_simulation.IsSimulationRunning()
-        else:
-            print("Not connected")
+        return(self.plant_simulation.IsSimulationRunning())        
 
+    @check_connection    
     def loadModel(self, ref, value):
-        if self.is_connected is True:
-            self.plant_simulation.LoadModel(ref, value)
-        else:
-            print("Not connected")
+        self.plant_simulation.LoadModel(ref, value)
 
+    @check_connection    
     def newModel(self):
-        if self.is_connected is True:
-            self.plant_simulation.NewModel()
-        else:
-            print("Not connected")
-
+        self.plant_simulation.NewModel()
+    
+    @check_connection    
     def openConsoleLogFile(self, ref):
-        if self.is_connected is True:
-            self.plant_simulation.OpenConsoleLogFile(ref)
-        else:
-            print("Not connected")
+        self.plant_simulation.OpenConsoleLogFile(ref)
 
+    @check_connection   
     def quit(self):
-        if self.is_connected is True:
-            self.plant_simulation.Quit()
-        else:
-            print("Not connected")
+        self.plant_simulation.Quit()
 
+    @check_connection    
     def quitAfterTime(self, value):
-        if self.is_connected is True:
-            self.plant_simulation.QuitAfterTime(value)
-        else:
-            print("Not connected")
+        self.plant_simulation.QuitAfterTime(value)
 
+    @check_connection    
     def saveModel(self, ref):
-        if self.is_connected is True:
-            self.plant_simulation.SaveModel(ref)
-        else:
-            print("Not connected")
+        self.plant_simulation.SaveModel(ref)
 
+    @check_connection    
     def setLicenseType(self, ref):
-        if self.is_connected is True:
-            self.plant_simulation.SetLicenseType(ref)
-        else:
-            print("Not connected")
+        self.plant_simulation.SetLicenseType(ref)
 
+    @check_connection    
     def setNoMessageBox(self, value):
-        if self.is_connected is True:
-            self.plant_simulation.SetNoMessageBox(value)
-        else:
-            print("Not connected")
+        self.plant_simulation.SetNoMessageBox(value)
 
+    @check_connection    
     def setPathContext(self, ref):
-        if self.is_connected is True:
             self.plant_simulation.SetPathContext(ref)
-        else:
-            print("Not connected")
 
+    @check_connection        
     def setSuppressStartOf3D(self, value):
-        if self.is_connected is True:
-            self.plant_simulation.SetSuppressStartOf3D(value)
-        else:
-            print("Not connected")
+        self.plant_simulation.SetSuppressStartOf3D(value)
 
+    @check_connection    
     def setTrustModels(self, value):
-        if self.is_connected is True:
-            self.plant_simulation.SetTrustModels(value)
-        else:
-            print("Not connected")
+        self.plant_simulation.SetTrustModels(value)
 
+    @check_connection    
     def transferModel(self, value):
-        if self.is_connected is True:
-            self.plant_simulation.TransferModel(value)
-        else:
-            print("Not connected")
-
-
-class connecterror(object):
-    def __init__(self, f):
-        self.f = f
-        self.name = f.__name__
-
-    def __call__(self, *args, **kwargs):
-        if self.is_connected is True:
-            self.f(*args, **kwargs)
-        else:
-            raise ConnectionError("Not connected:", self.name)
+        self.plant_simulation.TransferModel(value)
