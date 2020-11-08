e39ba056e5e70e62714fbc36045945d404d38af0
Carolina Saavedra Sueldo <carosaavedrasueldo@gmail.com>
2020-11-08 16:49:17 -0300

    arreglos

---
+++ docs/source/api/simulai.rst
@@ -0,0 +1,29 @@
+simulai package
+===============
+
+Submodules
+----------
+
+simulai.interface module
+------------------------
+
+.. automodule:: simulai.interface
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+simulai.sim module
+------------------
+
+.. automodule:: simulai.sim
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: simulai
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- simulai/interface.py
+++ simulai/interface.py
@@ -30,7 +30,7 @@ def check_connection(method):
     """
     def wrapper(self, *args, **kwargs):
         if not self.is_connected:
-            raise ConnectionError("Not connected", self.name)
+            raise ConnectionError("Not connected", self.model_name)
         output = method(self, *args, **kwargs)
         return output
     return wrapper
@@ -51,8 +51,9 @@ class Com(object):
         Connection status
     """
     def __init__(self, model_name):
-        self.model_name = model_name
+        self.model_name = model_name        
         self.is_connected = False
+        self.plant_simulation = ""
 
     def get_path_file_model(self):
         """
