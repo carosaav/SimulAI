da3ec44202907f9c11d1cbbdc8800d69af5db705
Ivo Perez Colo <ivoperezcolo@gmail.com>
2020-11-08 14:23:27 -0300

    Test connection_check

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -49,11 +49,7 @@ def test_connection(mock_method):
 	assert isinstance(retr, bool)
 
 
-@patch.object(interface.Com, 'connection', return_value=random.choice([True, False]))
-def test_setVible(mock_method1):
-	retr = interface.Com.connection()
-	mock_method1.assert_called_with()
-	method = interface.Com.setVisible(retr)
-	visible = interface.check_connection(method)
+def test_setVisible(base):
+	visible = base.setVisible()
 
-	assert isinstance(visible, bool)
+	assert isinstance(visible.value, bool)
