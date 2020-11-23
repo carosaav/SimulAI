a7ee67b1ac5642c885ba099b6f86e52fe7424f49
Ivo Perez Colo <ivoperezcolo@gmail.com>
2020-11-23 11:08:44 -0300

    Style test_integration

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -59,31 +59,44 @@ class Test_Com:
         # Step 1- Definition of input variables
 
         espera = simulai.DiscreteVariable(
-            "Espera", 60, 300, 10, "Models."+frame+".espera")
+            "Espera", 60, 300, 10, "Models." + frame + ".espera"
+        )
         stock = simulai.DiscreteVariable(
-            "Stock", 10, 50, 10, "Models."+frame+".stock")
+            "Stock", 10, 50, 10, "Models." + frame + ".stock"
+        )
         numviajes = simulai.DiscreteVariable(
-            "Numero de viajes", 1, 5, 1, "Models."+frame+".numviajes")
+            "Numero de viajes", 1, 5, 1, "Models." + frame + ".numviajes"
+        )
 
         var_input = [espera, stock, numviajes]
 
         # Step 2- Definition of output variables
 
         transportes = simulai.OutcomeVariable(
-            "Distancia Transportes", "Models."+frame+".transportes", 2, 9)
+            "Distancia Transportes", "Models." + frame + ".transportes", 2, 9
+        )
         buffers = simulai.OutcomeVariable(
-            "Llenado buffers", "Models."+frame+".buffers", 3, 20)
+            "Llenado buffers", "Models." + frame + ".buffers", 3, 20
+        )
         salidas = simulai.OutcomeVariable(
-            "Espera en las Salidas", "Models."+frame+".salidas", 2, 20)
+            "Espera en las Salidas", "Models." + frame + ".salidas", 2, 20
+        )
 
         var_output = [transportes, buffers, salidas]
 
         # Step 3- Choice of method, plant and simulation file.
         # Enter input variables and output variables.
 
-        my_method = simulai.Qlearning(v_i=var_input, episodes_max=5, steps_max=10)
-        my_plant = simulai.BasePlant(method=my_method, v_i=var_input, v_o=var_output,
-                                     filename="MaterialHandling.spp", modelname=frame)
+        my_method = simulai.Qlearning(
+            v_i=var_input, episodes_max=5, steps_max=10
+        )
+        my_plant = simulai.BasePlant(
+            method=my_method,
+            v_i=var_input,
+            v_o=var_output,
+            filename="MaterialHandling.spp",
+            modelname=frame,
+        )
 
         # Step 4- Run the simulation
 
@@ -104,22 +117,28 @@ class Test_Com:
         # Step 1- Definition of input variables
 
         espera = simulai.DiscreteVariable(
-            "Espera", 60, 300, 10, "Models."+frame+".espera")
+            "Espera", 60, 300, 10, "Models." + frame + ".espera"
+        )
         stock = simulai.DiscreteVariable(
-            "Stock", 10, 40, 10, "Models."+frame+".stock")
+            "Stock", 10, 40, 10, "Models." + frame + ".stock"
+        )
         numviajes = simulai.DiscreteVariable(
-            "Numero de viajes", 1, 4, 1, "Models."+frame+".numviajes")
+            "Numero de viajes", 1, 4, 1, "Models." + frame + ".numviajes"
+        )
 
         var_input = [espera, stock, numviajes]
 
         # Step 2- Definition of output variables
 
         transportes = simulai.OutcomeVariable(
-            "Distancia Transportes", "Models."+frame+".transportes", 2, 9)
+            "Distancia Transportes", "Models." + frame + ".transportes", 2, 9
+        )
         buffers = simulai.OutcomeVariable(
-            "Llenado buffers", "Models."+frame+".buffers", 3, 20)
+            "Llenado buffers", "Models." + frame + ".buffers", 3, 20
+        )
         salidas = simulai.OutcomeVariable(
-            "Espera en las Salidas", "Models."+frame+".salidas", 2, 20)
+            "Espera en las Salidas", "Models." + frame + ".salidas", 2, 20
+        )
 
         var_output = [transportes, buffers, salidas]
 
@@ -127,8 +146,13 @@ class Test_Com:
         # Enter input variables and output variables.
 
         my_method = simulai.Sarsa(v_i=var_input, episodes_max=10, steps_max=10)
-        my_plant = simulai.BasePlant(method=my_method, v_i=var_input, v_o=var_output,
-                                     filename="MaterialHandling.spp", modelname=frame)
+        my_plant = simulai.BasePlant(
+            method=my_method,
+            v_i=var_input,
+            v_o=var_output,
+            filename="MaterialHandling.spp",
+            modelname=frame,
+        )
 
         # Step 4- Run the simulation
 
